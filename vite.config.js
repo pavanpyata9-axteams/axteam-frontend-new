e8ed97fe364b7b881e65afb0b80b6977b0fa4f90
pavanpyata9-axteams <pavanpyata9@gmail.com>
2025-11-12 14:02:56 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,25 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000,
-  }
+  },
+  build: {
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        assetFileNames: (assetInfo) => {
+          const info = assetInfo.name.split('.');
+          const extType = info[info.length - 1];
+          if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType)) {
+            return `assets/images/[name].[hash][extname]`;
+          }
+          if (/mp4|webm|ogg|mp3|wav|flac|aac/i.test(extType)) {
+            return `assets/media/[name].[hash][extname]`;
+          }
+          return `assets/[name].[hash][extname]`;
+        },
+      },
+    },
+  },
+  assetsInclude: ['**/*.png', '**/*.jpg', '**/*.jpeg', '**/*.gif', '**/*.svg', '**/*.mp4', '**/*.webm']
 })
 
